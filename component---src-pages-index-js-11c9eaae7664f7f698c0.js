5a50b97f3aa52ebc84d8919c0a7b8fcc76cb6f39
mrlar <n@laplanete.ca>
2019-03-03 19:40:43 +0000

    Updates

---
+++ component---src-pages-index-js-11c9eaae7664f7f698c0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(142);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"Index and test auto deploy"}),r.a.createElement("p",null,"Such wow. Very React."))}},142:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,"Oh boy ",e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-index-js-11c9eaae7664f7f698c0.js.map
