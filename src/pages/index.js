f5b875fd331c22f5ef983aebdd47ea39ffd94f8b
Nicolas Desjardins <n@laplanete.ca>
2019-03-03 14:33:15 -0500

    test auto deploy circleci

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="Index" />
+    <Header headerText="Index and test auto deploy" />
     <p>Such wow. Very React.</p>
   </div>
 )
