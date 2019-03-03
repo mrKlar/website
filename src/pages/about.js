ff3abb8f07f1f6a234136ee5dec2804f3a33948d
Nicolas Desjardins <n@laplanete.ca>
2019-03-03 14:37:30 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
+    <Header headerText="About Gatsby test auto deploy" />
     <p>Such wow. Very React.</p>
   </div>
 )
